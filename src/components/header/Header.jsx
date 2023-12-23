0a19008e91611b7d9616ca916b54829ee68c689f
Boris Labianca <boris.labianca@hotmail.fr>
2023-12-23 16:23:18 +0800

    created header and footer components

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,46 @@
+import { BiLogIn } from "react-icons/bi";
+import { FaUserCircle } from "react-icons/fa";
+import "./Header.scss";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+
+const activeLink = ({ isActive }) => (isActive ? "active" : "");
+
+const Header = () => {
+  const navigate = useNavigate();
+  const goHome = () => {
+    navigate("/");
+  };
+
+  return (
+    <header className="header">
+      <nav>
+        <div className="logo" onClick={goHome}>
+          <BiLogIn size={35} />
+          <span>AdvncdAUTH</span>
+        </div>
+
+        <ul className="home-links">
+          <li className="--flex-center">
+            <FaUserCircle size={20} />
+            <p className="--color-white">Hi, Bobs</p>
+          </li>
+          <li>
+            <button className="--btn --btn-primary">
+              <Link to="/login">Login</Link>
+            </button>
+          </li>
+          <li>
+            <NavLink to="/profile" className={activeLink}>
+              Profile
+            </NavLink>
+          </li>
+          <li>
+            <button className="--btn --btn-secondary">Logout</button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
