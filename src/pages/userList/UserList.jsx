b9595081dd7b462afe8cbeefc30b2cb056bef6de
Boris Labianca <boris.labianca@hotmail.fr>
2023-12-25 14:29:39 +0800

    created user table, change role component and loader component

--- src/pages/userList/UserList.jsx
+++ src/pages/userList/UserList.jsx
@@ -1,7 +1,9 @@
+import { FaTrashAlt } from "react-icons/fa";
 import PageMenu from "../../components/pageMenu/PageMenu";
 import Search from "../../components/search/Search";
 import UserStats from "../../components/userStats/UserStats";
 import "./UserList.scss";
+import ChangeRole from "../../components/changeRole/ChangeRole";
 
 const UserList = () => {
   return (
@@ -10,13 +12,43 @@ const UserList = () => {
         <PageMenu />
         <UserStats />
         <div className="user-list">
-          <div className="--flex-between">
-            <span>
-              <h3>All Users</h3>
-            </span>
-            <span>
-              <Search />
-            </span>
+          <div className="table">
+            <div className="--flex-between">
+              <span>
+                <h3>All Users</h3>
+              </span>
+              <span>
+                <Search />
+              </span>
+            </div>
+            <table>
+              <thead>
+                <tr>
+                  <th>S/N</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Role</th>
+                  <th>Change Role</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Bob</td>
+                  <td>bob@mail.com</td>
+                  <td>Admin</td>
+                  <td>
+                    <ChangeRole />
+                  </td>
+                  <td>
+                    <span>
+                      <FaTrashAlt size={20} color="red" />
+                    </span>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
